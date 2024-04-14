ec568411246cdb053cb8e6aa19af4a4019f2bee4
Ryan Unroe <ryanunroe@gmail.com>
2024-04-14 12:43:49 -0600

    Set up page data structure

---
+++ src/pageData.ts
@@ -0,0 +1,73 @@
+export default {
+  home: {
+    imageSrc: "",
+    headline: "Director  |  Stage Manager  |  Actor  |  Singer  |  Artist",
+    bio: "Janell is a senior at the University of Utah working on her BA in theatre studies. She is very excited to be a part of helping create this amazing show as an Assistant Director. Some of her work includes being the dramaturg for The Lightning Thief: The Percy Jackson Musical at the University of Utah, directing Gay Girls with Umbrella Theatre Company in The New Works Mentorship Project,  Near Mint- a new production with the New Play Workshop class in the Department of Theatre, and directed a short version of The Tempest for Egyptian Youtheatre."
+  },
+  experience: {
+    "directing": {
+      coverImageSrc: "",
+      projects: [
+        {
+          title: "",
+          imageSource: "",
+          dateString: "",
+          description: "",
+        }
+      ]
+    },
+    "stage-managing": {
+      coverImageSrc: "",
+      projects: [
+        {
+          title: "",
+          imageSource: "",
+          dateString: "",
+          description: "",
+        }
+      ]
+    },
+    "acting": {
+      coverImageSrc: "",
+      projects: [
+        {
+          title: "The Little Mermaid",
+          imageSource: "",
+          dateString: "07 · 05 · 2022",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed imperdiet elit. Pellentesque maximus, nunc eget porttitor convallis, mi eros gravida dolor, vel pharetra sapien purus quis enim. Praesent malesuada, purus sed mattis posuere, erat justo scelerisque metus, a convallis quam arcu sed lacus. Donec imperdiet risus vel tellus vulputate, sed mollis nulla pellentesque. Nullam elementum sapien felis, id tempor diam mattis nec. Cras sit amet nisl mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris porttitor odio metus, eget scelerisque dui vulputate nec. In maximus interdum vehicula. Nullam vulputate feugiat tincidunt. Donec varius libero eu diam vestibulum, porttitor varius erat ornare.",
+        }
+      ]
+    }
+  },
+  resume: {
+    pdf: {
+      src: "",
+      name: "janell-rogers-resume.pdf",
+    }
+  },
+  contact: {
+    imageSrc: "",
+    phoneNumber: "+1 (808) 783-8941",
+    email: "kjre00@gmail.com",
+    socials: [
+      {
+        title: "Facebook",
+        username: "", //Use this with title to make image alt e.g. (Facebook - @JRogers)
+        iconSrc: "",
+        url: "",
+      },
+      {
+        title: "Instagram",
+        username: "",
+        iconSrc: "",
+        url: "",
+      },
+      {
+        title: "Email",
+        username: "",
+        iconSrc: "",
+        url: "",
+      },
+    ]
+  }
+}
