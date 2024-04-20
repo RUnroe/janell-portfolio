0232cac1f1a366ce0f45a49f2894f461fe0e2e68
Ryan Unroe <ryanunroe@gmail.com>
2024-04-20 17:56:12 -0600

    Get text animation working

---
+++ src/components/text/AnimatedTitle.tsx
@@ -0,0 +1,93 @@
+//https://codesandbox.io/p/sandbox/framer-motion-responsive-text-animation-z7udk?file=%2Fsrc%2FAnimatedText.js%3A12%2C18
+//Code by nathansearles (modified)
+
+
+import { motion } from "framer-motion";
+import ComponentWrapper from "../util/ComponentWrapper";
+
+// Word wrapper
+const Wrapper = (props:any) => {
+  // We'll do this to prevent wrapping of words using CSS
+  return <span className="word-wrapper">{props.children}</span>;
+};
+
+// Map API "type" vaules to JSX tag names
+const tagMap: { [key: string]: string  } = {
+  p: "p",
+  h1: "h1",
+  h2: "h2"
+};
+
+// AnimatedCharacters
+// Handles the deconstruction of each word and character to setup for the
+// individual character animations
+interface Props {
+  type: string,
+  text: string,
+}
+const AnimatedCharacters = (props: Props) => {
+  // Framer Motion variant object, for controlling animation
+  const item = {
+    hidden: {
+      y: "200%",
+      color: "#0055FF",
+      transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.85 }
+    },
+    visible: {
+      y: 0,
+      color: "#FF0088",
+      transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 0.75 }
+    }
+  };
+
+  //  Split each word of props.text into an array
+  const splitWords = props.text.split(" ");
+
+  // Create storage array
+  const words: Array<Array<string>> = [];
+
+  // Push each word into words array
+  for (const [, item] of splitWords.entries()) {
+    words.push(item.split(""));
+  }
+
+  // Add a space ("\u00A0") to the end of each word
+  words.map((word) => {
+    return word.push("\u00A0");
+  });
+
+  // Get the tag name from tagMap
+  const Tag = tagMap[props.type] as keyof JSX.IntrinsicElements;
+
+  return (
+    <ComponentWrapper tag={Tag}>
+      {words.map((word, index) => {
+        return (
+          // Wrap each word in the Wrapper component
+          <Wrapper key={index}>
+            {words[index].flat().map((element: string, index: number) => {
+              return (
+                <span
+                  style={{
+                    overflow: "hidden",
+                    display: "inline-block"
+                  }}
+                  key={index}
+                >
+                  <motion.span
+                    style={{ display: "inline-block" }}
+                    variants={item}
+                  >
+                    {element}
+                  </motion.span>
+                </span>
+              );
+            })}
+          </Wrapper>
+        );
+      })}
+    </ComponentWrapper>
+  );
+};
+
+export default AnimatedCharacters;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,35 @@ import pageData from "../pageData";
 
 import headshot from "../assets/images/headshot.jpg";
 import SocialRow from "../components/socials/SocialRow";
+import AnimatedCharacters from "../components/text/AnimatedTitle";
+import { motion } from "framer-motion";
 
 const Home = () => {
+
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025
+      }
+    }
+  };
 
   return (
     <PageLayout pageName="home-page">
       
         <header className="header">
           <h1 className="text-uppercase text-large"><TextHighlight>Hello, </TextHighlight><TextHighlight> I'm Janell Rogers</TextHighlight></h1>
+          
         </header>
+        <motion.header 
+          className="header"
+          initial="hidden"
+          animate="visible"
+          // animate={replay ? "visible" : "hidden"}
+          variants={container}
+        >
+          <AnimatedCharacters type="h1" text="Hello, I'm animated"/>
+        </motion.header>
 
         <aside className="headshot">
           <img className="dropshadow border-radius" src={headshot} />
