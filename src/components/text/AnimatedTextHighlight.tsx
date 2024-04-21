daeaabda841d420297e4e20fb180543bab1bc034
Ryan Unroe <ryanunroe@gmail.com>
2024-04-21 17:47:04 -0600

    Try to fix weird highlight jumping bug

--- src/components/text/AnimatedTextHighlight.tsx
+++ src/components/text/AnimatedTextHighlight.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 interface Props {
   containerClassName?: string,
@@ -8,7 +8,18 @@ interface Props {
 }
 
 const AnimatedTextHighlight = ({containerClassName = '', children, delayOffset = 0} : Props) => {
-  const variants = {
+  //Delay added this way to try to accomodate for title text width calculation time. 
+  //The title text width starts larger than it needs to be. It gets adjusted, but this highlight-block takes the first value, so it extends too far
+  const highlightDelay = 200; //ms
+  const [showHighlight, setShowHighlight] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowHighlight(true);
+    }, highlightDelay)
+  }, []);
+
+  let variants = {
     hidden: {
       width: 0,
       transition: { delay: 0, ease: [0.1, 0.03, 0.515, 0.955], duration: 0.75 }
@@ -24,11 +35,13 @@ const AnimatedTextHighlight = ({containerClassName = '', children, delayOffset =
   return ( 
     <motion.div className={`text-highlight animated ${containerClassName}`}>
       {children}
+      {showHighlight ? 
       <motion.div 
         className="hightlight-block"
         
         variants={variants}
       ></motion.div>
+      : null}
     </motion.div>
    );
 }
