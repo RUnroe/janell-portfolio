20ff00e20284a06a7afbd4d443bcb4c242e76f56
Ryan Unroe <ryanunroe@gmail.com>
2024-11-11 16:13:37 -0700

    Add directing projects

--- src/pageData.ts
+++ src/pageData.ts
@@ -12,15 +12,107 @@ export default {
       coverImageSrc: "/directing.jpg",
       projects: [
         {
-          title: "",
-          subline: [""],
+          title: "Alma and How She Got her Name",
+          subline: ["Director for Egyptian Youtheatre", "Book written by Juana Martinez-Neal · Adapted by Janell Rogers"],
+          media: [
+            {
+              type: "image",
+              src: "/projects/alma/Directing ALMA.jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/alma/Directing ALMA(1).jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/alma/Directing ALMA(2).jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/alma/Directing ALMA(3).jpg",
+            },
+          ],
+          dateString: "Jun 2024",
+          description: "Collaborated with bilingual students by adapting the story to create an inclusive and engaging storytelling experience that celebrated diverse cultures and languages. This approach fostered a supportive environment where students could contribute their unique perspectives, enhancing the narrative and promoting cultural understanding.",
+        },
+        {
+          title: "Dancing at Lughnasa",
+          subline: ["Written By Brian Friel · Directed by Sarah Shippobotham", "University of Utah"],
+          media: [
+            {
+              type: "image",
+              src: "/projects/dancing-at-lughnasa/Assitant Directing DANCING.jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/dancing-at-lughnasa/Assitant Directing DANCING(1).jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/dancing-at-lughnasa/Assitant Directing DANCING(2).jpg",
+            },
+          ],
+          dateString: "Feb - Mar 2024",
+          description: "Worked as secondary director specifically for the understudy cast",
+        },
+        {
+          title: "Tempest",
+          subline: ["Director for Egyptian Youtheatre", "Written by William Shakespeare · Adapted by Janell Rogers"],
           media: 
             {
               type: "image",
               src: "",
             },
-          dateString: "",
-          description: "",
+          dateString: "Jun 2023",
+          description: "Collaborated with youth by adapting the story to introduce a whimsical and imaginative point of view to the original version. This process involved interactive workshops where participants shared ideas, added creative twists, and explored unique character perspectives. The collaborative effort not only made the story more playful and engaging but also encouraged the youth to express themselves, think creatively, and develop a sense of ownership over the storytelling process.",
+        },
+        {
+          title: "Near Mint",
+          subline: ["By Lane Richins", "University of Utah"],
+          media: [
+            {
+              type: "image",
+              src: "/projects/near-mint/Directing Near Mint.jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/near-mint/Directing Near Mint(1).jpg",
+            },
+          ],
+          dateString: "Jan - Apr 2022",
+          description: "Collaborated closely with the writer and fellow classmates to finalize a staged reading of the play, contributing to refining the script through constructive feedback sessions and group discussions. Assisted in organizing rehearsals, aligning on creative vision, and making script adjustments to enhance dialogue flow and character development. This collaborative process fostered teamwork, improved the quality of the performance, and prepared the piece for presentation to an audience, ensuring a polished and engaging final reading.",
+        },
+        {
+          title: "Illyria",
+          subline: ["Assistant Directed", "Written By Peter Mills · Directed by Jason Snow", "University of Utah"],
+          media: [
+            {
+              type: "image",
+              src: "/projects/illyria/Assitant Directing ILLYRIA.jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/illyria/Assitant Directing ILLYRIA(1).jpg",
+            },
+            {
+              type: "image",
+              src: "/projects/illyria/Assitant Directing ILLYRIA(2).jpg",
+            },
+          ],
+          dateString: "Feb - Mar 2022",
+          description: "Worked to provide another point of view for the show as well as take down blocking.",
+        },
+       
+        {
+          title: "Gay Girls",
+          subline: ["Written By Makena Reynolds", "Umbrella Company"],
+          media: 
+            {
+              type: "image",
+              src: "/projects/gay-girls/musical-theatre.jpg",
+            },
+          dateString: "Dec 2021",
+          description: "Collaborated with youth and a writer to create a dynamic and engaging short scene within a tight timeframe of less than 6 hours. This involved brainstorming story ideas, developing characters, and crafting dialogue that resonated with the participants' perspectives and experiences. The process included quick scripting, revising, and rehearsing to ensure the scene was cohesive and compelling.",
         },
       ]
     },
@@ -59,7 +151,7 @@ export default {
               src: "/projects/hunchback-of-notre-dame/IMG_9367.jpg",
             },
           ],
-          dateString: "07 · 2024",
+          dateString: "Jul 2024",
           description: "The Hunchback of Notre Dame musical, based on Victor Hugo’s novel and Disney’s film, follows Quasimodo, the kind but lonely bell-ringer of Notre Dame, who seeks freedom and connection. His life changes when he meets Esmeralda, a brave Romani woman fighting for justice. With powerful songs like “Out There” and “God Help the Outcasts,” the musical explores themes of love, acceptance, and the battle between good and evil, captivating audiences with its rich characters and hauntingly beautiful music.",
         },
         {
@@ -79,7 +171,7 @@ export default {
               src: "/projects/cinderella/IMG_9362.jpg",
             },
           ],
-          dateString: "07 · 2023",
+          dateString: "Jul 2023",
           description: "Rodgers and Hammerstein’s Cinderella is a musical adaptation of the classic fairy tale, where a kind young woman, mistreated by her stepfamily, is transformed by her Fairy Godmother to attend a royal ball. There, she captures the heart of Prince Charming. Featuring beloved songs like “In My Own Little Corner” and “Ten Minutes Ago,” the musical highlights themes of hope, kindness, and believing in the impossible, enchanting audiences with its timeless story and memorable score.",
         },
         {
@@ -103,7 +195,7 @@ export default {
               src: "/projects/the-little-mermaid/IMG_9359.jpg",
             },
           ],
-          dateString: "07 · 2021",
+          dateString: "Jul 2021",
           description: "The Little Mermaid musical is a stage adaptation of Disney’s classic film, following Ariel, a young mermaid who dreams of life on land. Defying her father, King Triton, she makes a deal with the sea witch Ursula to trade her voice for human legs and pursue her love, Prince Eric. With iconic songs like “Part of Your World” and “Under the Sea,” the show explores themes of adventure, sacrifice, and following one's heart, captivating audiences with its vibrant characters and heartfelt story.",
         },
         {
@@ -116,7 +208,7 @@ export default {
             },
           
           ],
-          dateString: "08 · 2019",
+          dateString: "Aug - Oct 2019",
           description: "Mirror of Most Value: A Ms. Marvel Play is part of the Marvel Spotlight series, which brings superheroes into relatable, everyday situations. The play focuses on Kamala Khan, a young Pakistani-American teenager who gains polymorphic powers after being exposed to the Terrigen Mist. As Kamala embraces her new identity as Ms. Marvel, she becomes obsessed with building a superhero fandom by writing fan fiction. However, this obsession starts affecting her schoolwork and relationships. Kamala's journey teaches her the importance of self-acceptance and recognizing her own imperfections, all while navigating the challenges of being both a teenager and a superhero.",
         },
       ]
